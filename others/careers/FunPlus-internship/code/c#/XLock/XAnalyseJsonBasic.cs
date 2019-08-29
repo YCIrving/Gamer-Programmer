fd703eff4f026612a25889a740d11af6fea4910d
YCIrving <yichenirving@gmail.com>
2019-08-29 11:38:09 +0800

    移除涉及商业隐私的内容

--- others/careers/FunPlus-internship/code/c#/XLock/XAnalyseJsonBasic.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class XAnalyseJsonBasic
-{
-    public string err;
-    public string result;
-}
--- others/careers/FunPlus-internship/code/c#/XLock/XGlobalFlag.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class XGlobalFlag : MonoBehaviour {
-    public static bool xGlobalFlag= true;
-    public static bool xGlobalPostProcessFlag = false;
-}
